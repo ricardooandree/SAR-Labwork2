ee409ce66322dd3ec8d4225cd1efc62171edb245
pedromamaral <pedro.m.amaral@gmail.com>
2024-04-22 18:39:03 +0000

    fixed repeated code in socket.js

--- Backend/routes/socket.js
+++ Backend/routes/socket.js
@@ -55,17 +55,6 @@ exports.StartSocket = (io) => {
 
     ioSocket = io; // store socket object for use in interval (timer) function
     
-    io.use((socket, next) => {
-        if (socket.handshake.query && socket.handshake.query.token) {
-            jwt.verify(socket.handshake.query.token, secret, function (err, decoded) {
-                if (err) return next(new Error('Authentication error'));
-                socket.decoded_token = decoded;
-                next();
-            });
-        } else {
-            next(new Error('Authentication error'));
-        }
-    });
 
     //set up jwt authentication in the socket
     io.use((socket, next) => {
